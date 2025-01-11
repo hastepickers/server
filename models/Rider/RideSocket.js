1b287a8b8e1906ae9de3918b07caad0433f682be
Ibeneme <ibenemeikenna96@gmail.com>
2025-01-11 22:44:17 +0100

    ride-socket

--- models/Rider/RideSocket.js
+++ models/Rider/RideSocket.js
@@ -20,13 +20,39 @@ const RideSocketSchema = new mongoose.Schema(
       required: true,
     },
     pickup: {
-      type: String,
-      required: true,
+      pickupLatitude: { type: Number, required: true },
+      pickupLongitude: { type: Number, required: true },
+      pickupAddress: { type: String, required: true },
+      pickupTime: { type: Date, required: false },
     },
-    deliveryDropoff: {
-      type: String,
-      required: true,
-    },
+    deliveryDropoff: [
+      {
+        id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+        deliveryLatitude: { type: Number, required: true },
+        deliveryLongitude: { type: Number, required: true },
+        deliveryAddress: { type: String, required: true },
+        parcelId: {
+          type: String,
+          required: false,
+          default: function () {
+            return Math.random().toString(36).substr(2, 12).toUpperCase(); // Generate random parcel ID
+          },
+        },
+        receiverName: { type: String, required: true },
+        receiverPhoneNumber: { type: String, required: true },
+        receiverUserId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: false,
+        },
+        items: [
+          {
+            itemName: { type: String, required: true },
+          },
+        ],
+        price: { type: Number, required: false },
+      },
+    ],
     status: {
       type: String,
       required: true,
