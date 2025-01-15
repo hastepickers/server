ba791d9af3876cb347e31dff8ee7e2c5f9dd96a8
Ibeneme <ibenemeikenna96@gmail.com>
2025-01-15 18:59:04 +0100

    updates

--- routes/Rider/UserRiderRouter.js
+++ routes/Rider/UserRiderRouter.js
@@ -7,11 +7,13 @@ const {
   verifyOtpAndUpdatePhoneNumber,
   resendOtp,
   getRideSocketLogs,
+  getRideById,
 } = require("../../controllers/Rider/UserRiderController");
 const { verifyRiderTokenMiddleware } = require("../../utils/ridertokenUtil");
 
 // Protected routes
 router.get("/profile", verifyRiderTokenMiddleware, getRiderProfile);
+router.get("/get-a-ride/:id", verifyRiderTokenMiddleware, getRideById);
 router.put("/profile", verifyRiderTokenMiddleware, updateRiderProfile);
 router.put("/update-phone", verifyRiderTokenMiddleware, updatePhoneNumber);
 router.get("/ride-socket-logs", verifyRiderTokenMiddleware, getRideSocketLogs);
