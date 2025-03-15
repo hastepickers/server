f4fc37897097bb08baae490eeb1f2754655fbde6
Ibeneme <ibenemeikenna96@gmail.com>
2025-03-15 08:51:10 +0100

    updates

--- models/Rider/RiderEarnings.js
+++ models/Rider/RiderEarnings.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
 // Earnings Schema
-const EarningsSchema = new mongoose.Schema({
-  riderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Rider",
-    required: true,
+const EarningsSchema = new mongoose.Schema(
+  {
+    riderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Rider",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    header: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  header: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-}, {
-  timestamps: true // Automatically adds createdAt and updatedAt fields
-});
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
 
-const Earning = mongoose.model("Earning", EarningsSchema);
-module.exports = Earning;
+const Earnings = mongoose.model("Earnings", EarningsSchema);
+module.exports = Earnings;
