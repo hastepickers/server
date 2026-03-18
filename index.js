bcc1c9ae1e78d70ac0450cb7504a8b0122d9c0e1
ibeneme <ikennaibenemee@gmail.com>
2026-03-18 16:17:37 +0100

    updates

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose
-  .connect(process.env.MONGO_URIs, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
